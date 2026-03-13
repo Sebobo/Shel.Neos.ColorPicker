2c160e4a0636012e3966b0ec8a3a9c4e561884b9
Sebastian Helzle <sebastian@helzle.it>
2026-03-13 10:47:24 +0100

    BUGFIX: Generate unique css classnames

--- Resources/Private/Scripts/ColorPickerEditor/esbuild.js
+++ Resources/Private/Scripts/ColorPickerEditor/esbuild.js
@@ -1,4 +1,5 @@
 const esbuild = require('esbuild');
+const CssModulesPlugin = require('esbuild-css-modules-plugin');
 const extensibilityMap = require('@neos-project/neos-ui-extensibility/extensibilityMap.json');
 const isWatch = process.argv.includes('--watch');
 
@@ -12,6 +13,15 @@ const options = {
     loader: {
         '.js': 'tsx',
     },
+    plugins: [
+        CssModulesPlugin({
+            // @see https://github.com/indooorsman/esbuild-css-modules-plugin/blob/main/index.d.ts for more details
+            force: true,
+            localsConvention: 'camelCaseOnly',
+            namedExports: true,
+            inject: false,
+        }),
+    ],
     alias: extensibilityMap,
     outdir: '../../../Public/ColorPickerEditor',
 };
